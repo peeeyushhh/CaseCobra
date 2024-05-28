5207ea70663b7599fb7dae01d6265f30004e878e
peeeyushh <tanwarpiyushh@gmail.com>
2024-05-29 03:13:19 +0530

    initial

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,13 @@
+
+import React, { Suspense } from 'react'
+import ThankYou from './ThankYou'
+
+const Page = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  )
+}
+
+export default Page
