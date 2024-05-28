58ac41e9c6485104724e33b6047e3feadc3be88c
peeeyushh <tanwarpiyushh@gmail.com>
2024-05-29 04:05:58 +0530

    update stuff

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,16 +1,20 @@
-import { db } from "@/db"
-import { stripe } from "@/lib/stripe"
-import { headers } from "next/headers"
-import { NextResponse } from "next/server"
-import Stripe from "stripe"
+import { db } from '@/db'
+import { stripe } from '@/lib/stripe'
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+import { Resend } from 'resend'
+import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
 
-export async function POST(req:Request) {
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req: Request) {
   try {
     const body = await req.text()
     const signature = headers().get('stripe-signature')
 
-    if(!signature) {
-      return new Response('Invalid Signature', {status: 400})
+    if (!signature) {
+      return new Response('Invalid signature', { status: 400 })
     }
 
     const event = stripe.webhooks.constructEvent(
@@ -27,12 +31,12 @@ export async function POST(req:Request) {
       const session = event.data.object as Stripe.Checkout.Session
 
       const { userId, orderId } = session.metadata || {
-        userId:null,
-        orderId:null,
-      } 
+        userId: null,
+        orderId: null,
+      }
 
-      if(!userId || !orderId) {
-        throw new Error("Invalid request metadata")
+      if (!userId || !orderId) {
+        throw new Error('Invalid request metadata')
       }
 
       const billingAddress = session.customer_details!.address
@@ -66,11 +70,31 @@ export async function POST(req:Request) {
           },
         },
       })
+
+      await resend.emails.send({
+        from: 'CaseCobra <piyushtanwar859@gmail.com>',
+        to: [event.data.object.customer_details.email],
+        subject: 'Thanks for your order!',
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      })
     }
 
-    return NextResponse.json({result: event,ok: true})
+    return NextResponse.json({ result: event, ok: true })
   } catch (err) {
     console.error(err)
+
     return NextResponse.json(
       { message: 'Something went wrong', ok: false },
       { status: 500 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,9 +39,9 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: '@peeeyushhh',
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://case-cobra-livid.vercel.app/")
   }
 }
